c2cd02e7386e73e8003086524f5708cbb197d5e6
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2022-01-21 15:02:21 +0530

    fifth commit

--- src/screens/CallingScreen/index.js
+++ src/screens/CallingScreen/index.js
@@ -11,6 +11,7 @@ import styles from './styles';
 import CallActionBox from '../../components/CallActionBox';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useNavigation, useRoute} from '@react-navigation/core';
+import {Voximplant} from 'react-native-voximplant';
 
 const permissions = [
   PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
@@ -19,9 +20,15 @@ const permissions = [
 
 const CallingScreen = () => {
   const [permissionGranted, setPermissionGranted] = useState(false);
+
   const navigation = useNavigation();
+
   const route = useRoute();
+
+  const voximplant = Voximplant.getInstance();
+
   const user = route?.params.user;
+
   const goBack = () => {
     navigation.pop();
   };
@@ -29,6 +36,7 @@ const CallingScreen = () => {
   useEffect(() => {
     const getPermissions = async () => {
       const granted = await PermissionsAndroid.requestMultiple(permissions);
+      console.warn(granted);
       const recordAudioGranted =
         granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] === 'granted';
 
@@ -46,6 +54,48 @@ const CallingScreen = () => {
       setPermissionGranted(true);
     }
   }, []);
+
+  useEffect(() => {
+    if (!permissionGranted) {
+      return;
+    }
+
+    const callSettings = {
+      video: {
+        sendVideo: true,
+        receiveVideo: true,
+      },
+    };
+
+    let call;
+
+    const makeCall = async () => {
+      call = await voximplant.call(user.user_name, callSettings);
+      subscribeToCallEvents();
+    };
+
+    const subscribeToCallEvents = () => {
+      call.on(Voximplant.CallEvents.Failed, callEvent => {
+        showError(callEvent.reason);
+      });
+    };
+
+    const showError = reason => {
+      Alert.alert('Call failed', `Reason: ${reason}`, [
+        {
+          text: 'Ok',
+          onPress: navigation.navigate('Contacts'),
+        },
+      ]);
+    };
+
+    makeCall();
+
+    return () => {
+      call.off(Voximplant.CallEvents.Failed);
+    };
+  }, [permissionGranted]);
+
   return (
     <View style={styles.page}>
       <Pressable onPress={goBack} style={styles.backButton}>
